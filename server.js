30e584d58cc529de1121f9219988aeb98325a055
Greg <gsc229@gmail.com>
2019-10-30 13:04:17 -0400

    logging method and url visted from lectrue exercise

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const express = require('express'); // importing a CommonJS module
-
+const helmet = require('helmet');
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
+//the three amigos
+function dateLogger(req, res, next) {
+  console.log(new Date().toISOString());
+
+  next();
+}
+
+//global middleware: runs on every requests that comes into the server. 
+server.use(helmet()); // third party middleware
 server.use(express.json());
+server.use(dateLogger); //custom middleware -not invoked
+
+server.use('/', function (req, res, next) { // GET 'http://www.example.com/admin/new'
+  console.log(req.originalUrl) // '/admin/new'
+  console.log(req.baseUrl) // '/admin'
+  console.log(req.path) // '/new'
+  console.log(req.method)
+  next()
+})
 
 server.use('/api/hubs', hubsRouter);
 
