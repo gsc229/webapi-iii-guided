f68c91b664520085632e4901891eeff18fe9d583
Greg <gsc229@gmail.com>
2019-10-30 13:34:12 -0400

    gateKeeper code challenge

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express'); // importing a CommonJS module
 const helmet = require('helmet');
 const hubsRouter = require('./hubs/hubs-router.js');
+const morgan = require('morgan');
 
 const server = express();
 
@@ -10,11 +11,32 @@ function dateLogger(req, res, next) {
 
   next();
 }
+// change the gatekeeper to return a 400 if no password is provided and a message
+// that says please provide a password
+// if a password is provided and it is mellon, call next, otherwise return a 401
+// and the you shall not pass message
+function gateKeeper(req, res, next) {
+  //data can come in on the body, params, query string, headers
+  const password = req.headers.password;
+  if (!password) {
+    res.status(400).json({ message: "You did not provide a password" })
+    console.log("YOU DID PROVIDE A PASSWORD!")
+  }
+  else if (password.toLowerCase() === 'mellon') {
+    console.log("CORRECT PASSWORD! YOU SHALL PASS!!!")
+    next();
+  } else {
+    console.log("WRONG PASSWORD!")
+    res.status(401).json({ you: 'cannot pass!!!' });
+  }
+}
 
 //global middleware: runs on every requests that comes into the server. 
 server.use(helmet()); // third party middleware
 server.use(express.json());
+server.use(gateKeeper);
 server.use(dateLogger); //custom middleware -not invoked
+
 
 server.use('/', function (req, res, next) { // GET 'http://www.example.com/admin/new'
   console.log(req.originalUrl) // '/admin/new'
@@ -25,6 +47,8 @@ server.use('/', function (req, res, next) { // GET 'http://www.example.com/admin
 })
 
 server.use('/api/hubs', hubsRouter);
+server.use(morgan('dev'));
+
 
 server.get('/', (req, res) => {
   const nameInsert = (req.name) ? ` ${req.name}` : '';
